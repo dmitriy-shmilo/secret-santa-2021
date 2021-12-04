f76ac9879099118870f0b3a8e0e0b3519a14783d
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2021-12-04 23:06:15 +0200

    Mood gui.

--- src/game_screen/client.gd
+++ src/game_screen/client.gd
@@ -8,10 +8,16 @@ enum ClientState {
 }
 
 const MAX_MOOD = 100.0
+const GOOD_MOOD_OFFSET = 32
+const NORMAL_MOOD_OFFSET = GOOD_MOOD_OFFSET + 16
+const BAD_MOOD_OFFSET = GOOD_MOOD_OFFSET + 32
 
 signal mood_changed(current, total)
 
 var mood_decay_speed = 5.0
+
+onready var _mood_indicator: Sprite = $"MoodIndicator"
+onready var _mood_progress: TextureProgress = $"MoodProgress"
 
 var _mood = MAX_MOOD
 var _state = ClientState.OFF_SCREEN
@@ -19,6 +25,7 @@ var _state = ClientState.OFF_SCREEN
 func _process(delta) -> void:
 	if _state == ClientState.WAITING:
 		_mood = clamp(_mood - mood_decay_speed * delta, 0, MAX_MOOD)
+		_update_ui()
 		emit_signal("mood_changed", _mood, MAX_MOOD)
 
 
@@ -57,4 +64,14 @@ func generate_client() -> void:
 
 func _reset() -> void:
 	_mood = MAX_MOOD
+	_update_ui()
 	emit_signal("mood_changed", _mood, MAX_MOOD)
+
+func _update_ui() -> void:
+	_mood_progress.value = _mood
+	if _mood <= MAX_MOOD / 3.0:
+		_mood_indicator.region_rect.position.y = BAD_MOOD_OFFSET
+	elif _mood >= MAX_MOOD / 1.5:
+		_mood_indicator.region_rect.position.y = GOOD_MOOD_OFFSET
+	else:
+		_mood_indicator.region_rect.position.y = NORMAL_MOOD_OFFSET
